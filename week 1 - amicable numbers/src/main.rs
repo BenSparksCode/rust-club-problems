6c8e827a7768151f6c51f1bafa45f51d0a57d521
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2022-01-07 00:09:48 +0200

    More function scaffolding

--- week 1 - amicable numbers/src/main.rs
+++ week 1 - amicable numbers/src/main.rs
@@ -13,3 +13,21 @@ fn findDivisors(x: u32) -> std::vec::Vec<u32> {
     let mut i = 0;
     while i < (x.sqrt() + 1) {}
 }
+
+// If x is not amicable number, return (0,0)
+fn getAmicablePair(x: u32) -> (u32, u32) {
+    let x_factors: Vec<u32> = findDivisors(x);
+
+    // TODO sum factors to get y
+    let y: u32 = 0; // TODO change to sum
+
+    let y_factors: Vec<u32> = findDivisors(y);
+
+    let y_factor_sum = 0; // TODO sum
+
+    if (y_factor_sum != x) {
+        return (0, 0);
+    }
+
+    return (x, y);
+}
