4b028c64b6e1b2ebb5134cd887637dc44a65f904
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2022-01-07 00:19:45 +0200

    Added iteration logic to main

--- week 1 - amicable numbers/src/main.rs
+++ week 1 - amicable numbers/src/main.rs
@@ -2,11 +2,23 @@
 
 fn main() {
     println!("Hello, world!");
-    println!("{:?}", sum(4.0, 5.0))
-}
 
-fn sum(x: f32, y: f32) -> f32 {
-    return x + y;
+    let mut i: i32 = 0;
+    let mut x: u32 = 0;
+    let mut y: u32 = 0;
+    let mut sum: u32 = 0;
+
+    for i in 2..10000 {
+        (x, y) = getAmicablePair(i);
+        if x == 0 {
+            break;
+        }
+        // TODO store x,y as amicables in hash map (to skip second check)
+        sum += x + y;
+        println!("Amicable: {} and {}", x, y);
+    }
+
+    println!("{:?}", sum)
 }
 
 fn findDivisors(x: u32) -> std::vec::Vec<u32> {
@@ -26,7 +38,7 @@ fn getAmicablePair(x: u32) -> (u32, u32) {
     let y_factor_sum = 0; // TODO sum
 
     if (y_factor_sum != x) {
-        return (0, 0);
+        return (u32::MIN, u32::MIN);
     }
 
     return (x, y);
